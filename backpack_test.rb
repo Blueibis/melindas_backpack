65528c1c78d951ee16814587cb6adc1ad6d07099
Blueibis <jennie.li.2011@gmail.com>
2017-06-26 14:52:10 -0400

    created gym shoes test; corrected gym shoes comment

--- backpack_test.rb
+++ backpack_test.rb
@@ -45,7 +45,11 @@ class BackpackTest < MiniTest::Test
 
   ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
   # is something missing here?
-
+  def test_that_brings_shoes_on_monday_thursday
+    backpack = build(:backpack, day_of_week: 'monday')
+    backpack = build(:backpack, day_of_week: 'thursday')
+    assert(backpack.items.include?('gym shoes'))
+  end
 
   ##### TESTS ABOUT PRINTING THE LIST #####
   def test_that_backpack_prints_correctly
